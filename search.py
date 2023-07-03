5760d3152f69ddef6951719582c361947183e4d7
rahil_wisdom <80165152+rahilwisdom@users.noreply.github.com>
2023-07-03 15:20:33 +0800

    Update search.py

--- search.py
+++ search.py
@@ -2,7 +2,6 @@ import pysolr
 
 # Configure connection to Apache solr
 solr_homedepo = pysolr.Solr('http://localhost:8983/solr/homedepo')
-solr_detail_product = pysolr.Solr('http://localhost:8983/solr/detail_product')
 
 def search_function(core, query, rows = 12 ):
     results = core.search(query, rows = rows)
